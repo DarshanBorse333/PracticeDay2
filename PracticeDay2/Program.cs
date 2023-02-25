55a86805a61a12dbb9954d5beda22158247dbd81
DarshanBorse333 <Darshanborse@gmail.com>
2023-02-25 12:31:11 +0530

    Add project files.

---
+++ PracticeDay2/Program.cs
@@ -0,0 +1,103 @@
+﻿
+public class Program
+{
+    public static void Main()
+    {
+        NestedLoop1();
+        NestedLoop2();
+        NestedLoop3();
+        NestedLoop4();
+        NestedLoop5();
+        NestedLoop6();
+    }
+
+    static void NestedLoop1()
+    {
+
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 1; j <= 5; j++)
+            {
+                Console.Write(j);
+            }
+
+            Console.WriteLine();
+            
+        }
+        Console.WriteLine("-------------------------------------------");
+    }
+
+    static void NestedLoop2()
+    {
+        for (int i = 5; i >= 1; i--)
+        {
+            for (int j = 5; j >= 1; j--)
+            {
+                Console.Write(j);
+            }
+
+            Console.WriteLine();
+        }
+        Console.WriteLine("-------------------------------------------");
+    }
+
+    static void NestedLoop3()
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 1; j <= 5; j++)
+            {
+                Console.Write(i);
+            }
+
+            Console.WriteLine();
+        }
+        Console.WriteLine("-------------------------------------------");
+    }
+
+    static void NestedLoop4()
+    {
+        for (int i = 5; i >= 1; i--)
+        {
+            for (int j = 5; j >= 1; j--)
+            {
+                Console.Write(i);
+            }
+
+            Console.WriteLine();
+        }
+        Console.WriteLine("-------------------------------------------");
+    }
+
+    static void NestedLoop5()
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+        Console.WriteLine("-------------------------------------------");
+    }
+
+
+    static void NestedLoop6()
+    {
+        for (int i = 5; i >= 1; i--)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+        Console.WriteLine("-------------------------------------------");
+
+    }
+
+
+}
