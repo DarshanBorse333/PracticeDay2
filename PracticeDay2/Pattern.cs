db1640e34f6449b6d2a2417ce03558135e47d8bc
DarshanBorse333 <Darshanborse@gmail.com>
2023-02-26 11:58:37 +0530

    Patterns added

---
+++ PracticeDay2/Pattern.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeDay2
+{
+    internal class Pattern
+    {
+
+
+        public static void Pattern1()
+        {
+            for (int i = 5; i >= 1; i--)
+            {
+                for (int j = 5; j >= 1; j--)
+                {
+                    Console.Write(" "+Convert.ToChar(64+j));
+                }
+
+                Console.WriteLine();
+
+            }
+            Console.WriteLine("_________________________________________________");
+        }
+
+        public static void Pattern2()
+        {
+            for (int i = 5; i >= 1; i--)
+            {
+                for(int j = 1; j <= 6; j++)
+                {
+                    Console.Write(" " + Convert.ToChar(64 + i));
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
--- PracticeDay2/Program.cs
+++ PracticeDay2/Program.cs
@@ -1,103 +1,108 @@
 ﻿
+using PracticeDay2;
+
 public class Program
 {
     public static void Main()
     {
-        NestedLoop1();
-        NestedLoop2();
-        NestedLoop3();
-        NestedLoop4();
-        NestedLoop5();
-        NestedLoop6();
+        //NestedLoop1();
+        //NestedLoop2();
+        //NestedLoop3();
+        //NestedLoop4();
+        //NestedLoop5();
+        //NestedLoop6();
+        Pattern.Pattern1();
+        Pattern.Pattern2();
+
     }
 
-    static void NestedLoop1()
-    {
+    //static void NestedLoop1()
+    //{
 
-        for (int i = 1; i <= 5; i++)
-        {
-            for (int j = 1; j <= 5; j++)
-            {
-                Console.Write(j);
-            }
+    //    for (int i = 1; i <= 5; i++)
+    //    {
+    //        for (int j = 1; j <= 5; j++)
+    //        {
+    //            Console.Write(j);
+    //        }
 
-            Console.WriteLine();
+    //        Console.WriteLine();
             
-        }
-        Console.WriteLine("-------------------------------------------");
-    }
+    //    }
+    //    Console.WriteLine("-------------------------------------------");
+    //}
 
-    static void NestedLoop2()
-    {
-        for (int i = 5; i >= 1; i--)
-        {
-            for (int j = 5; j >= 1; j--)
-            {
-                Console.Write(j);
-            }
+    //static void NestedLoop2()
+    //{
+    //    for (int i = 5; i >= 1; i--)
+    //    {
+    //        for (int j = 5; j >= 1; j--)
+    //        {
+    //            Console.Write(j);
+    //        }
 
-            Console.WriteLine();
-        }
-        Console.WriteLine("-------------------------------------------");
-    }
+    //        Console.WriteLine();
+    //    }
+    //    Console.WriteLine("-------------------------------------------");
+    //}
 
-    static void NestedLoop3()
-    {
-        for (int i = 1; i <= 5; i++)
-        {
-            for (int j = 1; j <= 5; j++)
-            {
-                Console.Write(i);
-            }
+    //static void NestedLoop3()
+    //{
+    //    for (int i = 1; i <= 5; i++)
+    //    {
+    //        for (int j = 1; j <= 5; j++)
+    //        {
+    //            Console.Write(i);
+    //        }
 
-            Console.WriteLine();
-        }
-        Console.WriteLine("-------------------------------------------");
-    }
+    //        Console.WriteLine();
+    //    }
+    //    Console.WriteLine("-------------------------------------------");
+    //}
 
-    static void NestedLoop4()
-    {
-        for (int i = 5; i >= 1; i--)
-        {
-            for (int j = 5; j >= 1; j--)
-            {
-                Console.Write(i);
-            }
+    //static void NestedLoop4()
+    //{
+    //    for (int i = 5; i >= 1; i--)
+    //    {
+    //        for (int j = 5; j >= 1; j--)
+    //        {
+    //            Console.Write(i);
+    //        }
 
-            Console.WriteLine();
-        }
-        Console.WriteLine("-------------------------------------------");
-    }
+    //        Console.WriteLine();
+    //    }
+    //    Console.WriteLine("-------------------------------------------");
+    //}
 
-    static void NestedLoop5()
-    {
-        for (int i = 1; i <= 5; i++)
-        {
-            for (int j = 1; j <= i; j++)
-            {
-                Console.Write("*");
-            }
+    //static void NestedLoop5()
+    //{
+    //    for (int i = 1; i <= 5; i++)
+    //    {
+    //        for (int j = 1; j <= i; j++)
+    //        {
+    //            Console.Write("*");
+    //        }
 
-            Console.WriteLine();
-        }
-        Console.WriteLine("-------------------------------------------");
-    }
+    //        Console.WriteLine();
+    //    }
+    //    Console.WriteLine("-------------------------------------------");
+    //}
 
 
-    static void NestedLoop6()
-    {
-        for (int i = 5; i >= 1; i--)
-        {
-            for (int j = 1; j <= i; j++)
-            {
-                Console.Write("*");
-            }
+    //static void NestedLoop6()
+    //{
+    //    for (int i = 5; i >= 1; i--)
+    //    {
+    //        for (int j = 1; j <= i; j++)
+    //        {
+    //            Console.Write("*");
+    //        }
 
-            Console.WriteLine();
-        }
-        Console.WriteLine("-------------------------------------------");
+    //        Console.WriteLine();
+    //    }
+    //    Console.WriteLine("-------------------------------------------");
 
-    }
+    //}
 
 
 }
